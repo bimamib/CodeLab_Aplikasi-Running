e41e7f6b946a969dfc1a59eebdbb0a7c4b1cb1bf
bimamib <bimaprast1601@gmail.com>
2023-10-20 01:44:49 +0700

    upgrade compileSdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.bima.mylocationtracker"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.bima.mylocationtracker"
