e64d58afb3567214b2e13566835a77a91e25e849
bimamib <bimaprast1601@gmail.com>
2023-10-20 19:54:07 +0700

    update sedikit

--- app/src/main/java/com/bima/mylocationtracker/MapsActivity.kt
+++ app/src/main/java/com/bima/mylocationtracker/MapsActivity.kt
@@ -1,29 +1,26 @@
 package com.bima.mylocationtracker
 
+import android.Manifest
+import android.content.IntentSender
 import android.content.pm.PackageManager
 import android.location.Location
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.IntentSenderRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import com.bima.mylocationtracker.databinding.ActivityMapsBinding
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import com.bima.mylocationtracker.databinding.ActivityMapsBinding
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
-import android.Manifest
-import android.content.IntentSender
-import android.icu.util.TimeUnit
-import android.location.LocationRequest
-import android.util.Log
-import androidx.activity.result.IntentSenderRequest
-import com.google.android.gms.common.api.ResolvableApiException
-import com.google.android.gms.location.LocationSettingsRequest
+import java.util.concurrent.TimeUnit
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -47,22 +44,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-
-        // Add a marker in Sydney and move the camera
-//        val sydney = LatLng(-34.0, 151.0)
-//        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
 
 //        getMyLastLocation()
         createLocationRequest()
@@ -146,12 +129,14 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                     ).show()
             }
         }
+
     private fun createLocationRequest() {
         locationRequest = LocationRequest.create().apply {
             interval = TimeUnit.SECONDS.toMillis(1)
             maxWaitTime = TimeUnit.SECONDS.toMillis(1)
             priority = LocationRequest.PRIORITY_HIGH_ACCURACY
         }
+
         val builder = LocationSettingsRequest.Builder()
             .addLocationRequest(locationRequest)
         val client = LocationServices.getSettingsClient(this)
